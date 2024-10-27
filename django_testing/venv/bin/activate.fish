bbea9051b7892b03c6deaff07c6d68fd8968618b
valexsav <valexvasilevich@gmail.com>
2024-10-27 19:21:13 +0100

    add GET request handling in index, returns a simple HttpResponse

--- django_testing/venv/bin/activate.fish
+++ django_testing/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alexo/Desktop/dj_0/venv"
+set -gx VIRTUAL_ENV "/home/alexo/Desktop/python_practice/django_testing/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
